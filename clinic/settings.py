fe71262a6731d277b42161343751ead4ae6aab25
apple <tutor.dalmas.otieno@gmail.com>
2021-06-27 18:51:52 +0300

    included the simple jwt token retrieval views and urls

--- clinic/settings.py
+++ clinic/settings.py
@@ -44,6 +44,12 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'user_management.CustomUser'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
